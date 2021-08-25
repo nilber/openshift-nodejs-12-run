01f363941a9eced98fb5d34d5bd8ff3a44185f34
nilber <nilber@vittorazzi.com>
2021-08-25 20:37:59 -0300

    bkp

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 DOCKER_IMAGEM=nodejs-12-run
 S2I_IMAGEM=nbmaster/openshift-nodejs-12-run
-TAG=1.1
+TAG=1.4
 
 # docker system prune
 # docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
 
-# docker build src/ -t $DOCKER_IMAGEM:$TAG
+docker build src/ -t $DOCKER_IMAGEM:$TAG
 
 s2i build src/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
 
